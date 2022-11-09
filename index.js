e4d8d8d1dfa8d876393bc2a5e16bbced46033ca6
CocoNixy <m.nicole1994@gmail.com>
2022-11-09 20:45:23 +0200

    JS intro

---
+++ index.js
@@ -0,0 +1,20 @@
+// let birthdate = 1994;
+// let thisyear = 2022;
+
+// let yourage = thisyear - birthdate;
+
+// alert(yourage);
+
+
+// let age = 80;
+// let thisyear = 2022;
+
+// let yourage = thisyear - age;
+
+// alert(yourage);
+
+
+let firstName = 'Nikol';
+let lastName = 'Machlyciova';
+
+alert(firstName + ' ' + lastName);
