afaa0c5f6305006b2a807f2995b332001488223c
CocoNixy <m.nicole1994@gmail.com>
2023-01-10 21:57:48 +0200

    added loops (for)

---
+++ loops/loops.js
@@ -0,0 +1,12 @@
+//cycles
+
+const orderNumberOfCars = (numberOfCars) => {
+
+    for (let i = 1; i <= numberOfCars; i++) {
+        console.log(`car ${i} has been ordered`);
+    }
+    console.log('finished');
+}
+orderNumberOfCars(10);
+
+//type of cycles: for, while, do...while
